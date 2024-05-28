7b036c1e7f0c8553739bfebc387bf4141bc7ccc8
rebecaCaroba <rebecacaroba@gmail.com>
2024-05-28 10:11:13 -0300

    Criando componente aside

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
 import { DefaultLayout } from './Layout/DefaultLayout.tsx'
+import { Aside } from './components/Aside/index.tsx';
+import { Library } from './pages/Library/index.tsx';
 
 export function Router() {
+  const location = useLocation();
+  const showAside = location.pathname.startsWith('/library');
 
   return (
     <div className='app'>
+      {showAside && <Aside />}
+      <div  className={`content ${showAside ? 'with-sidebar' : ''}`}>
         <Routes>
           <Route path='/' element={<DefaultLayout />}>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/library" element={<Library />} />
           </Route>
         </Routes>
+      </div>
     </div>
   )
 }
---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import './style.scss'
+
+export function Aside() {
+    const [activeLink, setActiveLink] = useState(null);
+
+  const handleLinkClick = (link: any) => {
+    setActiveLink(link);
+  };
+
+    return (
+    <aside>
+        <div className="sidebar-logo">
+            <Link to="/library"><h2>Logo</h2></Link>
+        </div>
+      <nav className="sidebar-nav">
+        <ul>
+          <li>
+            <Link
+            className={activeLink === 'library' ? 'active' : ''}
+            onClick={() => handleLinkClick('library')}
+            to="/library">Página inicial</Link>
+          </li>
+          <li>
+            <Link
+            className={activeLink === 'library/add-collection' ? 'active' : ''}
+            onClick={() => handleLinkClick('library/add-collection')} 
+            to="/library/add-collection">Adicionar coleção</Link>
+          </li>
+          <li>
+            <Link
+            className={activeLink === '/library/add-book' ? 'active' : ''}
+            onClick={() => handleLinkClick('/library/add-book')} 
+            to="/library/add-book">Adicionar livro</Link>
+          </li>
+          <li>
+            <Link
+            className={activeLink === '/library/dashboards' ? 'active' : ''}
+            onClick={() => handleLinkClick('/library/dashboards')} 
+            to="/library/dashboards">Painéis</Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+    )
+}
